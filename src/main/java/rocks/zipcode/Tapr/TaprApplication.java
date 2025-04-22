5b64d84b0aaf8f5ee9da0f108292eb418708b5e5
GabrielC67 <gabecruzpro@gmail.com>
2025-04-22 13:44:46 -0400

    First official commit

---
+++ src/main/java/rocks/zipcode/Tapr/TaprApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.Tapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaprApplication.class, args);
+	}
+
+}
