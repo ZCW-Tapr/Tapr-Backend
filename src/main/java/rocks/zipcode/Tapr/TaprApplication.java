c41d2a78aaeb4b00df36eb41a70007c5075ee57b
GabrielC67 <gabecruzpro@gmail.com>
2025-04-23 22:47:39 -0400

    Final commit for today

--- src/main/java/rocks/zipcode/Tapr/TaprApplication.java
+++ src/main/java/rocks/zipcode/Tapr/TaprApplication.java
@@ -21,73 +21,64 @@ public class TaprApplication {
 	@Bean
 	public CommandLineRunner user(UserRepository repository) {
 		return (args) -> {
-			repository.save(new User(1, "KDot234", "kris@zcw.com", "Kris", "Younger", "K.2345", 302-763-3333));
-			repository.save(new User(2, "Gpop67", "gcru123@zcw.net", "Gabe", "Cruz", "G.1234", 302-763-2234));
-			repository.save(new User(3, "PMango", "MangoP543@zcw.net", "Paulie", "Mango", "P543c278?", 302-763-6677));
-		};
+			repository.save(new User(1, "KDot234", "kris@zcw.com", "Kris", "Younger", "K.2345", 302 - 763 - 3333));
+			repository.save(new User(2, "Gpop67", "gcru123@zcw.net", "Gabe", "Cruz", "G.1234", 302 - 763 - 2234));
+			repository.save(new User(3, "PMango", "MangoP543@zcw.net", "Paulie", "Mango", "P543c278?", 302 - 763 - 6677));
 
+			// fetch all users
+			log.info("Users found with findAll():");
+			log.info("-------------------------------");
+			repository.findAll().forEach(user -> {
+				log.info(user.toString());
+			});
+			log.info("");
 
-		// fetch all users
-		log.info("Users found with findAll():");
-		log.info("-------------------------------");
-		repository.findAll().forEach(user -> {
+			// fetch an individual user by ID
+			User user = repository.findById(2);
+			log.info("User found with findById(2):");
+			log.info("--------------------------------");
 			log.info(user.toString());
-		});
-		log.info("");
+			log.info("");
 
-		// fetch an individual user by ID
-		User user = repository.findById(2);
-		log.info("User found with findById(2):");
-		log.info("--------------------------------");
-		log.info(user.toString());
-		log.info("");
+			// fetch users by username
+			log.info("User found with findByUsername('KDot234'):");
+			log.info("--------------------------------------------");
+			repository.findByUsername("KDot234").forEach(KDot234 -> {
+				log.info(KDot234.toString());
+			});
+			log.info("");
 
-		// fetch users by username
-		log.info("User found with findByUsername('KDot234'):");
-		log.info("--------------------------------------------");
-		repository.findByUsername("KDot234").forEach(KDot234 -> {
-			log.info(KDot234.toString());
-		});
-		log.info("");
+			// fetch users by email
+			log.info("User found with findByEmail('kris@zcw.com'):");
+			log.info("--------------------------------------------");
+			repository.findByEmail("kris@zcw.com").forEach(Kris -> {
+				log.info("kris@zcw.com".toString());
+			});
+			log.info("");
 
-		// fetch users by email
-		log.info("User found with findByEmail('kris@zcw.com'):");
-		log.info("--------------------------------------------");
-		repository.findByEmail("kris@zcw.com").forEach(Kris -> {
-			log.info("kris@zcw.com".toString());
-		});
-		log.info("");
+			// fetch users by first name
+			log.info("User found with findByFirstName('Kris'):");
+			log.info("--------------------------------------------");
+			repository.findByFirstName("Kris").forEach(Kris -> {
+				log.info(Kris.toString());
+			});
+			log.info("");
 
-		// fetch users by first name
-		log.info("User found with findByFirstName('Kris'):");
-		log.info("--------------------------------------------");
-		repository.findByFirstName("Kris").forEach(Kris -> {
-			log.info(Kris.toString());
-		});
-		log.info("");
+			// fetch users by last name
+			log.info("User found with findByLastName('Younger'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Younger").forEach(Younger -> {
+				log.info(Younger.toString());
+			});
+			log.info("");
 
-		// fetch users by password
-		log.info("User found with findByPassword('K.2345'):");
-		log.info("--------------------------------------------");
-		repository.findByPassword("K.2345").forEach(KDot234 -> {
-			log.info(KDot234.toString());
-		});
-		log.info("");
-
-		// fetch users by last name
-		log.info("User found with findByLastName('Younger'):");
-		log.info("--------------------------------------------");
-		repository.findByLastName("Younger").forEach(Younger -> {
-			log.info(Younger.toString());
-		});
-		log.info("");
-
-		// fetch users by phone number
-		log.info("User found with findByPhoneNumber('302-763-3333'):");
-		log.info("--------------------------------------------");
-		repository.findByPhoneNumber("302-763-3333").forEach(Kris -> {
-			log.info("302-763-3333".toString());
-		});
-		log.info("");
-	};
+			// fetch users by phone number
+			log.info("User found with findByPhoneNumber('302-763-3333'):");
+			log.info("--------------------------------------------");
+			repository.findByPhoneNumber("302-763-3333").forEach(Kris -> {
+				log.info("302-763-3333".toString());
+			});
+			log.info("");
+		};
+	}
 }
